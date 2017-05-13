ee791897da3f0522302efffcadd9ac97ad855699
nuttawoot <nuttawoot.s@app-alive.com>
2017-05-13 10:38:05 +0700

    echo server name

--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -4,7 +4,7 @@ class SimpleTest extends \PHPUnit_Framework_TestCase
 {
      public function testSimple1()
      {
-      $this->assertEquals(2, 1 + 2);
+      $this->assertEquals(3, 1 + 2);
      }
      public function testSimple2()
      {
