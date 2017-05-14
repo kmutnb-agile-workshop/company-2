a1510b736e790ca34363c8dafcd5c7af56453eca
Kraiwoot.l <kraiwoot.l@gmail.com>
2017-05-14 14:44:07 +0700

    [Update] function topup balance

--- postTopup.php
+++ postTopup.php
@@ -1,12 +1,7 @@
 <?php
-ob_start();
-session_start();
-require 'vendor/autoload.php';
-
-// init flag
-$action = 'add';
-$isSuccess = false;
-
-// get data from POST param
-$customerId = isset($_POST['customerId']) ? $_POST['customerId'] : '';
-$customerName = $_POST['customerName'];
+require "./src/DbConnection.php";
+    $db = DbConnection::getConnection();
+    $rs = $db->Execute("UPDATE User SET Balance = Balance + ".$_POST['amount']." WHERE Tel='".$_POST['tel']."'");
+    if($rs){
+    	echo " การเติมเงินเสร็จสมบูรณ์";
+    }
