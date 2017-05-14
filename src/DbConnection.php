cc24783ab461183618f93ba9b773c07025dab0c4
Kraiwoot.l <kraiwoot.l@gmail.com>
2017-05-14 15:10:49 +0700

    db

--- src/DbConnection.php
+++ src/DbConnection.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php';
 
 final class DbConnection {
   public static function getConnection () {
-    $development = array("driver"=>"mysqli", "server"=>"localhost", "user"=>"smart_growth_admin", "password"=>"8ezjkoMYsql", "database"=>"smart_growth");
+    $development = array("driver"=>"mysqli", "server"=>"localhost", "user"=>"root", "password"=>"Zaq12wsx", "database"=>"agile-php");
     $production = array("driver"=>"mysqli", "server"=>"54.169.158.243", "user"=>"root", "password"=>"1qaz2wsx", "database"=>"agile-php");
     
     // echo "server name is: ".$_SERVER['SERVER_NAME'];
